2ddb0f7e00c6598a9cfe5401a4d5618bc8e5e372
wakkyb <109560612+wakkyb@users.noreply.github.com>
2023-10-10 22:41:20 +0500

    Add files via upload

---
+++ 1. Two Sum.py
@@ -0,0 +1,17 @@
+# input ==> an array
+# Output ==> indices of the two numbers whose sum = target
+
+
+nums = [2,7,11,15]
+final = 9
+
+def two_sum(nums, target):
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        if target - num in num_to_index:
+            return [num_to_index[target - num], i]
+        num_to_index[num] = i
+    return []
+
+
+print(two_sum(nums, final))
